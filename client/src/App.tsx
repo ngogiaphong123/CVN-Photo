7bef874f54ea38c93783b465b6fe03b6cc6cc851
ngogiaphong123 <ngogiaphong123@gmail.com>
2023-12-01 13:00:12 +0700

    FE: setup router, aixos, env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
+
 function App() {
-  return <>hello world</>
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<div>About</div>} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,15 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import path from 'path'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+      '@redux': path.resolve(__dirname, './src/redux'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@components': path.resolve(__dirname, './src/components'),
     },
   },
 })
